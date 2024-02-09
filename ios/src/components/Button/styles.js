318dd7a3df7ff7cbe2730a817121f488cb0e0c0b
Kristjan <kristjan.puusepp@voco.ee>
2024-02-09 10:56:19 +0200

    did the splash thingy and lookin good

---
+++ ios/src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#4f63ac',
+        paddingVertical: 20,
+        paddingHorizontal: 8,
+        width: "100%",
+        borderRadius: 10
+    },
+    title: {
+        color: "#fff",
+        textAlign: "center",
+        fontSize: 16,
+        fontWeight: "bold"
+    }
+})
---
+++ ios/src/screens/auth/Splash/styles.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 200
+    },
+    title: {
+        textAlign: "center",
+        fontSize: 40,
+        fontWeight: "bold"
+    },
+    innerTitle: {
+        color: '#fca34d',
+        textDecorationLine: "underline"
+    },
+    container: {
+        alignItems: "center",
+        height: "100%",
+        justifyContent: "center",
+        flexDirection: "column",
+        padding: 24,
+    },
+    footerText: {
+        marginTop: 30,
+        color: "#4f63ac",
+        textAlign: "center",
+        fontSize: 16,
+        fontWeight: "bold"
+    },
+    titleContainer: {
+        marginVertical: 54
+    }
+
+})
