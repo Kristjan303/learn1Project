7e3ac02e0a7f6796506e2146cf7e088bf829e1ce
Kristjan <kristjan.puusepp@voco.ee>
2024-02-26 11:50:09 +0200

    ropemaxxing atm 3.

--- src/screens/app/Home/index.js
+++ src/screens/app/Home/index.js
@@ -1,13 +1,33 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "./styles"
+import Header from "../../../components/Header";
+import { categories } from "../../../data/categories";
+import CategoryBox from "../../../components/CategoryBox";
+import { products } from "../../../data/products";
+import ProductHomeItem from "../../../components/ProductHomeItem";
+
 
 const Home = () => {
+    const renderCategoryItem = ({item}) => {
+        console.log("item => ", item)
+        return (
+            <CategoryBox title={item?.title} image={item?.image}/>
+        )
+    }
+    const renderProductItem = ({item}) => {
+        return (
+            <ProductHomeItem {...item}/>
+        )
+    }
+
     return (
         <SafeAreaView>
             <View style={styles.container}>
-                <Text>Home</Text>
+                <Header showSearch={true} title="Find all you need"/>
+                <FlatList showsHorizontalScrollIndicator={false} horizontal style={styles.list} data={categories} renderItem={renderCategoryItem} keyExtractor={(item, index) => String(index)} />
+                <FlatList data={products} renderItem={renderCategoryItem} keyExtractor={(item) => String(item.id)}/>
             </View>
         </SafeAreaView>
     )
--- src/screens/app/Home/styles.js
+++ src/screens/app/Home/styles.js
@@ -1,9 +1,17 @@
 import { StyleSheet } from "react-native";
 import { create } from "react-test-renderer";
+import { colors } from "../../../utils/colors";
 
 
 export const styles = StyleSheet.create({
-    
+    container: {
+        padding: 24,
+
+    },
+    list: {
+        paddingVertical: 24,
+        color: colors.black
+    }
 
 })
 
