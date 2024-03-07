13558db23f2708759dbbb5b1bb0c5ad8c2ce7c41
Kristjan <kristjan.puusepp@voco.ee>
2024-03-07 12:14:57 +0200

    Create profile screen

--- src/screens/app/Profile/styles.js
+++ src/screens/app/Profile/styles.js
@@ -3,6 +3,22 @@ import { create } from "react-test-renderer";
 
 
 export const styles = StyleSheet.create({
-
-
+    container: {
+        padding: 24,
+        flex: 1
+    },
+    name: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: colors.black,
+        marginBottom: 12
+    },
+    email: {
+        color: colors.grey,
+        fontSize: 14,
+        marginBottom: 16
+    },
+    content: {
+        flex: 1,
+    }
 })
